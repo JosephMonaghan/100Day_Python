79bee1adfcb1ab1d8db2b652983be13a7fd17f29
JosephMonaghan <Joseph_Monaghan@outlook.com>
2024-04-22 20:42:17 -0700

    Made dashed line square functionality in sandbox.py

--- 018_Day/sandbox.py
+++ 018_Day/sandbox.py
@@ -7,15 +7,33 @@ timmy.color("black", "red")
 #timmy.circle(100)
 
 
-def draw_square(target, size):
-    """takes a turtle object (target) and draws a square of length (size) on screen"""
+def draw_square(target, size, dash_state=False):
+    """takes a turtle object (target) and draws a square of length (size) on screen. Optionally takes 'dash state to dash lines'"""
     for _ in range(4):
-        target.forward(size)
+        if not dash_state:
+            target.forward(size)
+        else:
+            dist = 0
+            if size > 0:
+                dash_length = 10
+            else:
+                dash_length = -10
+            
+            residual= size % dash_length
+            while dist < abs(size - dash_length):
+                target.forward(dash_length)
+                target.pu()
+                target.forward(dash_length)
+                target.pd()
+                dist += 2*abs(dash_length)
+            target.forward(residual)
+
+
         target.left(90)
 
 
 draw_square(timmy, 100)
-draw_square(timmy,-200)
+draw_square(timmy,-200,True)
 
 
 
