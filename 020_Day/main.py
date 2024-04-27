0d0375b122ddea6eb0fb5f0dc12567a3d39a2f15
JosephMonaghan <Joseph_Monaghan@outlook.com>
2024-04-27 15:23:01 -0700

    moved snake to a separate class

--- 020_Day/main.py
+++ 020_Day/main.py
@@ -1,11 +1,13 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 INITIAL_LENGTH=3
 INITIAL_X=0
 INITIAL_Y=0
 INITIAL_TRAJECTORY="west"
 trajectory=INITIAL_TRAJECTORY
+snake_length=INITIAL_LENGTH
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -13,23 +15,6 @@ screen.bgcolor("black")
 screen.title("Snake Game v1")
 screen.tracer(False)
 screen.listen()
-
-
-my_snake=[]
-def start_up():
-    '''Draws the initial snake at the center position, with tail going to the left'''
-    global INITIAL_LENGTH, INITIAL_X, INITIAL_Y
-    for i in range(INITIAL_LENGTH):
-        new_seg=Turtle(visible=False)
-        new_seg.speed(0)
-        new_seg.pu()
-        new_seg.setpos(INITIAL_X - i*20,INITIAL_Y)
-        new_seg.shape('square')
-        new_seg.color('white')
-        new_seg.speed(5)
-        new_seg.showturtle()
-        my_snake.append(new_seg)
-        screen.update()
 
 def go_north():
     global trajectory
@@ -51,9 +36,9 @@ def go_east():
     trajectory='east'
     my_snake[0].setheading(180)
 
-start_up()
-snake_length=INITIAL_LENGTH
-trajectory="west"
+
+snake = Snake()
+screen.update()
 
 screen.onkeypress(key="w",fun=go_north)
 screen.onkeypress(key="a",fun=go_east)
@@ -62,12 +47,7 @@ screen.onkeypress(key="d",fun=go_west)
 
 game_is_on=True
 while game_is_on:
-    replace_pos=my_snake[0].pos()
-    my_snake[0].forward(20)
-    for seg in range(1,len(my_snake)):
-        tmp=my_snake[seg].pos()
-        my_snake[seg].setpos(replace_pos)
-        replace_pos=tmp
+    snake.move()
     time.sleep(0.1)
     screen.update()
     
---
+++ 020_Day/snake.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+class Snake:
+    def __init__(self, initial_length=3, init_y=0, init_x=0):
+        self.seg_list=[]
+        for i in range(initial_length):
+            new_seg=Turtle(visible=False)
+            new_seg.speed(0)
+            new_seg.pu()
+            new_seg.setpos(init_x - i*20,init_y)
+            new_seg.shape('square')
+            new_seg.color('white')
+            new_seg.speed(5)
+            new_seg.showturtle()
+            self.seg_list.append(new_seg)
+    
+    def move(self):
+        replace_pos=self.seg_list[0].pos()
+        self.seg_list[0].forward(20)
+        for seg in range(1,len(self.seg_list)):
+            tmp=self.seg_list[seg].pos()
+            self.seg_list[seg].setpos(replace_pos)
+            replace_pos=tmp
