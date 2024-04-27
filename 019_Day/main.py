0b5ed95f1e4737e34d34ca2d5f788a37cc96dedd
JosephMonaghan <Joseph_Monaghan@outlook.com>
2024-04-27 13:14:57 -0700

    Added randomization to turtle headings, spruced up winning text :)

--- 019_Day/main.py
+++ 019_Day/main.py
@@ -25,15 +25,26 @@ leader=""
 
 while max_pos < 200:
     for turtle in range(len(turtle_racers)):
+        turtle_racers[turtle].pd()
         turtle_racers[turtle].forward(random.randint(0,10))
         cur_pos = turtle_racers[turtle].pos()
         if cur_pos[0] > max_pos:
             max_pos=cur_pos[0]
             leader=turtle_racers[turtle].color()
 
+        #Random headings so the turtles are wobbly :)
+        cur_heading=turtle_racers[turtle].heading()
+        if cur_heading == 0:
+            turtle_racers[turtle].setheading(0 + random.randint(0,15))
+        elif cur_heading > 0 and cur_heading < 35:
+            turtle_racers[turtle].right(random.randint(0,15))
+        elif cur_heading > 340:
+            turtle_racers[turtle].left(random.randint(0,15))
+        
+
 if user_bet.lower==leader[0].lower:
     print(f"Awesome, you won! The {leader[0]} turtle won the race.")
 else:
-    print(f"Sorry, you lost (womp womp). The {leader[0]} turtle won the race.")
+    print(f"Sorry, you lost (womp womp). The {user_bet} turtle lost and the {leader[0]} turtle won the race.")
 
 screen.exitonclick()
