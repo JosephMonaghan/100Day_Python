fb700171393b166641d9dc320867c8fcb03b2812
JosephMonaghan <Joseph_Monaghan@outlook.com>
2024-04-27 14:00:33 -0700

    Progress on controls and snake motion

--- 020_Day/main.py
+++ 020_Day/main.py
@@ -3,6 +3,8 @@ from turtle import Turtle, Screen
 INITIAL_LENGTH=3
 INITIAL_X=0
 INITIAL_Y=0
+INITIAL_TRAJECTORY="west"
+trajectory=INITIAL_TRAJECTORY
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -14,15 +16,65 @@ def start_up():
     '''Draws the initial snake at the center position, with tail going to the left'''
     global INITIAL_LENGTH, INITIAL_X, INITIAL_Y
     for i in range(INITIAL_LENGTH):
-        my_snake.append(Turtle(visible=False))
-        my_snake[i].speed(0)
-        my_snake[i].pu()
-        my_snake[i].setpos(INITIAL_X - i*20,INITIAL_Y)
-        my_snake[i].shape('square')
-        my_snake[i].color('white')
-        my_snake[i].speed('fast')
-        my_snake[i].showturtle()
+        new_seg=Turtle(visible=False)
+        new_seg.speed(0)
+        new_seg.pu()
+        new_seg.setpos(INITIAL_X - i*20,INITIAL_Y)
+        new_seg.shape('square')
+        new_seg.color('white')
+        new_seg.speed('fast')
+        new_seg.showturtle()
+        my_snake.append(new_seg)
+
+def go_north():
+    global trajectory
+    trajectory='north'
+
+def go_south():
+    global trajectory
+    trajectory='south'
+
+def go_west():
+    global trajectory
+    trajectory='west'
+
+def go_east():
+    global trajectory
+    trajectory='east'
 
 start_up()
+snake_length=INITIAL_LENGTH
+trajectory="west"
+
+game_is_on=True
+while game_is_on:
+    new_seg=Turtle(shape='square',visible=False)
+    new_seg.speed(0)
+    new_seg.pu()
+    new_seg.color('white')
+    head_pos=my_snake[0].pos()
+    if trajectory == "west":
+        new_seg.setpos(head_pos[0] + 20,head_pos[1])
+    elif trajectory == "east":
+        new_seg.setpos(head_pos[0] - 20, head_pos[1])
+    elif trajectory == "north":
+        new_seg.setpos(head_pos[0], head_pos[1]+20)
+    elif trajectory == "south":
+        new_seg.setpos(head_pos[0], head_pos[1]-20)
+    
+    new_seg.showturtle()
+    my_snake.insert(0,new_seg)
+    while len(my_snake) > snake_length:
+        my_snake.pop(-1)
+
+
+
+
+
+
+
+
+
+
 
 screen.exitonclick()
