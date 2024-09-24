3a0aa6c74efb0206d4bed4a19f62d10004576876
Joseph Monaghan <Joseph_Monaghan@outlook.com>
2024-09-23 18:37:13 -0700

    Basic framework for Day 53

---
+++ 053_Day/main.py
@@ -0,0 +1,12 @@
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import time, os
+import random
+
+load_dotenv()
+
+
+tgt_URL = "https://www.udemy.com/course/100-days-of-code/learn/lecture/21928006#questions/19522640"
