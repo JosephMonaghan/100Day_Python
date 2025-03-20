5b24f63b9c5b02756a6bfbc8319c8212008e2878
Joseph Monaghan <Joseph_Monaghan@outlook.com>
2025-03-20 10:28:24 -0700

    Rerolled secret key and moved to dotenv

--- 071_Day/day-71-starting-files-blog-for-deployment/main.py
+++ 071_Day/day-71-starting-files-blog-for-deployment/main.py
@@ -10,6 +10,8 @@ from sqlalchemy import Integer, String, Text
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from load_dotenv import load_dotenv
+import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
 
@@ -26,10 +28,10 @@ pip3 install -r requirements.txt
 
 This will install the packages from the requirements.txt for this project.
 '''
-
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("secret_key")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
